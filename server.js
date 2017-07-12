b9001d99ecd9a0b26cec6cc4e2455213d55d1bce
Michael Coates <MichaelMCoates@gmail.com>
2017-07-11 21:41:47 -0400

    Add comments

--- server.js
+++ server.js
@@ -4,28 +4,34 @@ const request = require('request');
 
 const app = express();
 
+// Render index.html
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// Index.html requirements
+
 app.use('/lib', express.static(__dirname + '/lib'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
-// app.use(express.static(__dirname + '/../views/index.html'));
+// Keys
 
-var client_id = '1781c6c33491424f802591ae3b3f1559'; // Your client id
-var client_secret = '7ff5306f33f4435d80c461b80db3ad6f'; // Your secret
+var client_id = '1781c6c33491424f802591ae3b3f1559';
+var client_secret = '7ff5306f33f4435d80c461b80db3ad6f';
+
+// Auth Setup
 
 var authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
     'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
   },
-  form: {
-    grant_type: 'client_credentials'
-  },
+  form: { grant_type: 'client_credentials' },
   json: true
 };
+
+// Spotify Web API Client Credentials Auth + Request
 
 app.get('/cors', function (req, res) {
   const url = req.query.url;
@@ -34,9 +40,7 @@ app.get('/cors', function (req, res) {
       var token = tokenBody.access_token;
       var options = {
         url: url,
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
+        headers: { 'Authorization': 'Bearer ' + token },
         json: true
       };
 
@@ -44,21 +48,11 @@ app.get('/cors', function (req, res) {
         res.send(getBody);
       });
   });
-
-
-  //
-  //
-  // request(url, function (error, response, body) {
-  //   if (error) {
-  //     res.status(422).send(`Failed to connect to ${url}`);
-  //   } else {
-  //     res.send(body);
-  //   }
-  // });
 });
 
 
-// Start up the server
+// Server Start
+
 const port = (process.env.PORT || 8080);
 app.listen(port, function () {
   console.log(`CORS demo listening on port ${port}`);
